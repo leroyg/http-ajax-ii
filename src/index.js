c8897c7db1c0e7e1fd2b6a4d2a09cf12382528ad
leroyg <leroy.gardner@gmail.com>
2019-06-27 13:54:41 -0400

    all done, but getting 404. so done-ish

--- src/index.js
+++ src/index.js
@@ -4,19 +4,35 @@ import { BrowserRouter, Route, Link } from "react-router-dom"
 import Home from "./components/Home"
 import Trinkets from "./components/Trinkets"
 import Trinket from "./components/Trinket"
-import data from "./data"
+import axios from "axios"
+import Create from "./components/Create";
+import Edit from "./components/Edit";
 import "./styles.css"
 
 class App extends React.Component {
-	state = {
-		items: []
-	}
+    constructor() {
+        super()
+        this.state = {
+			items: []
+		}
+    }
+  
 
 	componentDidMount() {
-		this.setState({
-			items: data
-		})
-	}
+        axios.get('http:localhost:3333/items')
+        .then(response =>{
+            this.setState({
+                items: response.data
+            })
+        })
+        .catch(error => {
+            console.table('Error:', error)
+        })
+    }
+    
+    updateItems = (items) => {
+        this.setState({ items })
+    }
 
 	render() {
 		const { items } = this.state
@@ -24,16 +40,19 @@ class App extends React.Component {
 		return (
 			<div className="App">
 				<nav>
-					<h1 className="store-header">Jason's Trinkets</h1>
+					<h1 className="store-header">LeRoy's Trinkets</h1>
 					<div className="nav-links">
 						<Link to="/">Home</Link>
 						<Link to="/trinkets">Trinkets</Link>
+                        <Link to="/new">New</Link>
 					</div>
 				</nav>
 
 				<Route path="/" exact render={(props) => <Home {...props} items={items} />} />
 				<Route path="/trinkets" exact render={(props) => <Trinkets {...props} items={items} />} />
 				<Route path="/trinket/:id" render={(props) => <Trinket {...props} items={items} />} />
+                <Route path="/new" exact render={(props) => <Create {...props} updateItems={this.updateItems}  />} />
+                <Route path="/edit/:id" exact render={(props) => <Edit {...props} updateItems={this.updateItems}  />} />
 			</div>
 		)
 	}
